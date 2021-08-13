2d95802f73288c65d1e9b6aace790cdb2c87716d
sumex <haohuan630@163.com>
2021-08-13 20:16:44 +0800

    stats_fine_tuning

---
+++ isbs.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+
+python isbs.py -is /home/lthpc/sumex/code/20210807_ISBS/yolact/yolact/results/20210812_masks/score_t=0.005_top_k=15 -bs /home/lthpc/sumex/code/20210807_ISBS/ViBe/results/matchingThreshold=45 -o results/yolact_vibe -opt 0 -r 0.3
